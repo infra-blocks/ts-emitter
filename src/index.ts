990cc9f1a8701b8df6cfff5b9d2970fd97dd8c35
PhilLavoie <PhilLavoie@users.noreply.github.com>
2025-11-30 09:50:54 -0500

    exporting strategies module
    - This was always intended, hence the patch version.

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./emitter.js";
 export * from "./emitter-like.js";
 export * from "./events.js";
 export type { EventsListeners, ListenersInvocations } from "./listener.js";
+export * from "./strategies.js";
